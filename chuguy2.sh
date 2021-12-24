4ce78cd6f955132a0e2211d5ef7736c359cffcfd
renzokuden <ivchuguy66@gmail.com>
2021-12-24 15:22:40 +0200

    new SH script

---
+++ chuguy2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p chuguy/NNN
+mkdir -p chuguy/OOO/
+mkdir -p chuguy/PPP/gr1/
+mkdir -p chuguy/PPP/gr2/
+cd chuguy/PPP/gr1/
+touch anton.txt 
+echo "131 02.12 ivchuguy66@gmail.com">anton.txt
+cd .. 
+cat > ~/Документы/chuguy/PPP/gr2/chuguy.txt
+echo "131 02.12 ivchuguy66@gmail.com" >chuguy.txt
